34bb00a9d36985ac95e20d243c194c31eaba0744
siavash.soleymani <siavash.soleimani@snapp-box.com>
2020-06-06 23:03:14 +0430

    initial commit

---
+++ src/main/java/LiveStreamServer.java
@@ -0,0 +1,104 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class LiveStreamServer {
+    private Map<Long, Socket> publisherSockets = new ConcurrentHashMap<>();
+    private Map<Long, Socket> subscriberSockets = new ConcurrentHashMap<>();
+    private static ExecutorService executorService = Executors.newCachedThreadPool();
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+
+    public LiveStreamServer(int port) {
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+            System.out.println("Waiting for a client ...");
+            startAcceptingClients();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    private void startAcceptingClients() throws IOException {
+        executorService.submit(() -> {
+            while (true) {
+                Socket socket = server.accept();
+                in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                long id = Long.parseLong(in.readLine());
+                System.out.println("Client accepted");
+                if (id > 3000)
+                    publisherSockets.putIfAbsent(id, socket);
+                else
+                    subscriberSockets.putIfAbsent(id, socket);
+                CompletableFuture.runAsync(getRunnable(socket, id), executorService);
+            }
+        });
+    }
+
+    private Runnable getRunnable(Socket socket, long id) {
+        if (id > 3000) {
+            return () -> {
+                while (true) {
+                    try {
+                        DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                        if (dataInputStream.available() > 0) {
+                            Long recepientId;
+                            Long timestamp;
+                            String action;
+                            try {
+                                String data = dataInputStream.readLine();
+                                if (data.startsWith("h"))
+                                    continue;
+                                String[] split = data.split(",");
+                                recepientId = Long.valueOf(split[0]);
+                                action = split[1];
+                                timestamp = Long.valueOf(split[2]);
+                            } catch (Exception e) {
+                                System.out.println("Unknown message received");
+                                continue;
+                            }
+                            Socket subscriberSocket = subscriberSockets.get(recepientId);
+                            System.out.println(action + "," + timestamp);
+                            if (subscriberSocket == null) {
+                                System.out.println("Unknown receiver");
+                                continue;
+                            }
+                            OutputStream outputStream = subscriberSocket.getOutputStream();
+                            outputStream.write((action + "," + timestamp).getBytes());
+                            outputStream.flush();
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+        } else return () -> {
+            while (true) {
+                try {
+                    OutputStream outputStream = socket.getOutputStream();
+                    outputStream.write("h\n".getBytes());
+                    outputStream.flush();
+                    Thread.sleep(5000);
+                } catch (Exception e) {
+                    System.out.println("one subscriber has been disconnected");
+                    subscriberSockets.values().remove(socket);
+                    break;
+                }
+            }
+        };
+    }
+
+    public static void main(String args[]) {
+        new LiveStreamServer(5000);
+    }
+}
---
+++ src/main/java/Publisher.java
@@ -0,0 +1,78 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Publisher {
+    private static Socket socket = null;
+
+    public Publisher(String address, int port) throws IOException {
+        connect(address, port);
+        clientIntroduction();
+        startHeartBeat(address, port);
+        getFromInput();
+    }
+
+    private void getFromInput() {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String s = sc.nextLine();
+            sendAction(s, 2001l);
+        }
+    }
+
+    public static void sendAction(String action, Long recipientId) {
+        try {
+            OutputStream outputStream = socket.getOutputStream();
+            String message = recipientId + "," + action + "," + System.currentTimeMillis() + "\n";
+            outputStream.write(message.getBytes());
+            outputStream.flush();
+        } catch (IOException e) {
+            System.out.println("couldn't send action message");
+        }
+    }
+
+    private void clientIntroduction() throws IOException {
+        DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+        outputStream.write("3001\n".getBytes());
+        outputStream.flush();
+    }
+
+    private void startHeartBeat(String address, int port) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            while (true) {
+                try {
+                    OutputStream outputStream = socket.getOutputStream();
+                    outputStream.write("h\n".getBytes());
+                    outputStream.flush();
+                    Thread.sleep(5000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    connect(address, port);
+                }
+            }
+        });
+    }
+
+    public static void main(String args[]) throws IOException {
+        new Publisher("127.0.0.1", 5000);
+    }
+
+    private static void connect(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+        } catch (IOException e) {
+            System.out.println("cant connect to server :" + e.getMessage());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+            connect(address, port);
+        }
+    }
+} 
